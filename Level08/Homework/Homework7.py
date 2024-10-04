336e1867ac19efa8740b5150350e0b2b2fbbd7f2
Sirzi <nik7bik@gmail.com>
2024-10-04 14:49:22 +0400

    first commit

--- Level08/Homework/Homework7.py
+++ Level08/Homework/Homework7.py
@@ -1,6 +1,7 @@
 num_1 = input("What is your first number: ")
 num_2 = input("What is your second number: ")
 p = int(num_1) > int(num_2) 
-q = num_1 < num_2
+q = int(num_1) < int(num_2)
 P_and_not_Q = p or q
-print(P_and_not_Q)
+not_P_and_Q = q or p
+P_xor_Q = p and q
