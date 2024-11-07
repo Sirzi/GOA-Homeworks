5917a1414f40aa59bff739728932046eedf11c63
Sirzi <nik7bik@gmail.com>
2024-11-07 18:21:12 +0400

    first commit

---
+++ Level18/Classwork/Classwork.py
@@ -0,0 +1,13 @@
+
+numbers = [9, 5, 94, 711, 52, 96, 71, 8]
+
+
+smallest = numbers[0]
+
+for number in numbers:
+
+    if number < smallest:
+        smallest = number
+
+# ვბეჭდავთ ყველაზე პატარა ელემენტს
+print("სიის ყველაზე პატარა ელემენტია:", smallest)
