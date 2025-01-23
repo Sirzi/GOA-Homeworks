475d54bdd830f955348063019b659a2c1ee0006e
Sirzi <nik7bik@gmail.com>
2025-01-23 17:14:25 +0400

    first commit

--- Level38/Homework/Homework2.py
+++ Level38/Homework/Homework2.py
@@ -1,10 +1,7 @@
-chatgpt_info = {
-    "name": "ChatGPT",
-    "version": "v2",
-    "language": "Multilingual",
-    "capabilities": ["Answering questions", "Generating text", "Coding assistance", "Translation"],
-    "knowledge_cutoff": "2023-10",
-    "current_date": "2025-01-16"
+nikola_info = {
+    "name": "Nikola",
+    "language": "English",
+    "current_date": "2025-01-16",
 }
-keys = chatgpt_info.keys()
+keys = nikola_info.keys()
 print(keys)
