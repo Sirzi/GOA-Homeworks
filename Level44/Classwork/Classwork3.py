b1e3a91b19326ff47fb02b8f0a3a51983885a303
Sirzi <nik7bik@gmail.com>
2025-02-06 15:48:40 +0400

    first commit

---
+++ Level44/Classwork/Classwork3.py
@@ -0,0 +1,15 @@
+def human_years_cat_years_dog_years(human_years):
+    catYears = 15
+    dogYears = 15
+    humanYears = 1
+    if humanYears >= 2:
+        catYears += 9
+        dogYears += 9
+    if humanYears > 2:
+        catYears += (humanYears - 2) * 4
+        dogYears += (humanYears - 2) * 5
+
+    return [humanYears, catYears, dogYears]
+
+
+print(human_years_cat_years_dog_years(1)) 
---
+++ Level44/Homework/Homework4.py
@@ -0,0 +1,1 @@
+#CodewarSi gvqonda
